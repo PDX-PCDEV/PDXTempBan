7a8f0dde0d7fa9bd054cb8ad0004828e8da2d7f5
Minimal <mark_r_official@outlook.com>
2017-09-12 20:30:52 +0100

    Initial Commit

---
+++ src/net/paradox/tempban/Main.java
@@ -0,0 +1,21 @@
+package net.paradox.tempban;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by mark_ on 12/09/2017.
+ */
+public class Main extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
